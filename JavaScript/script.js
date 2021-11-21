240254bda7f7e69622ef0f106e7a1ce661e4bdc5
kozzztia <kozzztia@ukr.net>
2021-11-22 01:24:19 +0200

    secondTry

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
 
+    // const main = document.querySelector('.main__container');
+
+    // const biography = document.querySelector('.biography');
+    // const work = document.querySelector('.landing');
+    // const code = document.querySelector('.code');
+    // const contact = document.querySelector('.contact');
+
+    // biography.addEventListener('click', () =>{
+    //   main.style.right = '0';
+    // })
+    // work.addEventListener('click', () =>{
+    //   main.style.right = '100%';
+    // })
+    // code.addEventListener('click', () =>{
+    //   main.style.right = '200%';
+    // })
+
+      $('.biography').click(() =>{
+        $('.main__container').animate({'right':'0'}),100;
+      })
+      $('.landing').click(() =>{
+        $('.main__container').animate({'right':'100%'}),100;
+      })
+      $('.code').click(() =>{
+        $('.main__container').animate({'right':'200%'}),100;
+      })
+
+
+
+
 
 
 
