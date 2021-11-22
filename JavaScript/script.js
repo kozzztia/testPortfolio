b51841a2bd50887bd9904b520be4b00af6c142ff
kozzztia <kozzztia@ukr.net>
2021-11-22 10:41:07 +0200

    third

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,18 +16,38 @@ $(document).ready(function(){
     // code.addEventListener('click', () =>{
     //   main.style.right = '200%';
     // })
-
+    // ---------------------------------------------------------------main slider
+    let changeColor = function(element){
+      $('.header__btn').css({'backgroundColor':'#C4C4C4'});
+      $(element).css({'backgroundColor':'#CBE918'});
+      }
       $('.biography').click(() =>{
+        changeColor('.biography');
         $('.main__container').animate({'right':'0'}),100;
+
       })
       $('.landing').click(() =>{
+        changeColor('.landing');
         $('.main__container').animate({'right':'100%'}),100;
+
       })
       $('.code').click(() =>{
+        changeColor('.code');
         $('.main__container').animate({'right':'200%'}),100;
+
       })
-
-
+      // -------------------------------------------------------------header burger
+      $('.menu').click(() =>{
+        if($('.header__nav').css('height') == '50px'){
+          $('.header__nav').animate({height:'248px', opacity:0.9},100);
+          $('.menu').html('&#10006')
+        }
+        if($('.header__nav').css('height') == '248px'){
+          $('.header__nav').animate({height:'50px', opacity:1},100);
+          $('.menu').html('&#9776')
+        }
+      })
+ 
 
 
 
